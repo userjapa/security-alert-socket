ae5cc2be51d0a1cccaf19891340a71ff64b07b8f
= <willian.hsimabukuro@gmail.com>
2018-11-26 00:20:54 -0200

    changed productiuon port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 const app = express()
 
