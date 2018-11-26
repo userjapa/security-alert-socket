a0ed3178c832208803bcd1b131e417cdb7a94840
= <willian.hsimabukuro@gmail.com>
2018-11-25 23:45:24 -0200

    added socked

---
+++ index.js
@@ -0,0 +1,58 @@
+const http = require('http')
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const PORT = process.env.PORT
+
+const app = express()
+
+app.use(serveStatic(path.join(__dirname, 'static')))
+app.use(serveStatic(path.join(__dirname, 'node_modules/socket.io-client/dist')))
+
+const server = http.Server(app)
+const socket = require('socket.io')(server)
+const io = socket.of('/arduino')
+
+let connection = {
+  arduino: false,
+  client: false
+}
+
+let id = {}
+
+io.on('connection', socket => {
+  // socket.emit('news', { hello: 'world' })
+  socket.on('client_connected', () => {
+    connection['client'] = true
+    id[socket.id] = 'client'
+    socket.emit('arduino_connection', connection['arduino'])
+    socket.emit('client_connection', connection['client'])
+    console.log('Client connected');
+  })
+
+  socket.on('arduino_connected', () => {
+    connection['arduino'] = true
+    id[socket.id] = 'arduino'
+    socket.emit('client_connection', connection['client'])
+    socket.emit('arduino_connection', connection['arduino'])
+  })
+
+  socket.on('disconnect', () => {
+    const type = id[socket.id]
+    connection[type] = false
+    delete id[socket.id]
+    socket.emit(`${type}_connection`, connection[type])
+  })
+
+  socket.on('request_verification', () => {
+    socket.emit('verification_requested')
+  })
+
+  socket.on('verification_made', data => {
+    socket.emit('verification_result', data)
+  })
+
+})
+
+server.listen(PORT, () => console.log(`Listening at port ${PORT}`))
---
+++ static/assets/js/index.js
@@ -0,0 +1,28 @@
+const socket = io.connect('http://localhost:8080/arduino')
+let checkBtn = document.getElementById('check'),
+    resultTxt = document.getElementById('result')
+
+let arduino_connected = false
+
+// SOCKET START
+socket.on('connect', () => {
+  socket.emit('client_connected')
+})
+
+socket.on('arduino_connection', connection => {
+  arduino_connected = connection
+  checkBtn.disabled = !arduino_connected
+  console.log(arduino_connected);
+})
+
+socket.on('verification_result', data => {
+  console.log(data)
+})
+// SOCKET END
+
+// DOM START
+checkBtn.onclick = () => {
+  resultTxt.textContent = ''
+  socket.emit('request_verification')
+}
+// DOM END
